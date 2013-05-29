92f07ca66d320075a32c92e81f37526563c208d0
Geoff Lawler <glawler@tislabs.com>
2013-05-29 19:36:11 -0400

    initial checkin. some graphics. board is setup but no play or interaction. initial DB setup.

---
+++ chess_chess.tpl
@@ -0,0 +1,49 @@
+{OVERALL_GAME_HEADER}
+
+<!-- 
+--------
+-- BGA framework: © Gregory Isabelli <gisabelli@boardgamearena.com> & Emmanuel Colin <ecolin@boardgamearena.com>
+-- Chess implementation : © <Your name here> <Your email address here>
+-- 
+-- This code has been produced on the BGA studio platform for use on http://boardgamearena.com.
+-- See http://en.boardgamearena.com/#!doc/Studio for more information.
+-------
+
+    chess_chess.tpl
+    
+    This is the HTML template of your game.
+    
+    Everything you are writing in this file will be displayed in the HTML page of your game user interface,
+    in the "main game zone" of the screen.
+    
+    You can use in this template:
+    _ variables, with the format {MY_VARIABLE_ELEMENT}.
+    _ HTML block, with the BEGIN/END format
+    
+    See your "view" PHP file to check how to set variables and control blocks
+-->
+
+<div id="board">
+    <!-- BEGIN square -->
+        <div id="square_{X}_{Y}" class="square" style="left: {LEFT}px; top: {TOP}px;"></div>
+    <!-- END square -->
+    <div id="pieces"></div>
+</div>
+
+
+<script type="text/javascript">
+
+// Javascript HTML templates
+
+var jstp_piece='<div class="piece piece_${color}_${name}" id="piece_${x_y}"></div>';
+
+
+/*
+// Example:
+var jstpl_some_game_item='<div class="my_game_item" id="my_game_item_${id}"></div>';
+
+*/
+
+</script>  
+
+{OVERALL_GAME_FOOTER}
---
+++ stats.inc.php
@@ -0,0 +1,79 @@
+<?php
+
+/**
+ *------
+ * BGA framework: © Gregory Isabelli <gisabelli@boardgamearena.com> & Emmanuel Colin <ecolin@boardgamearena.com>
+ * Chess implementation : © <Your name here> <Your email address here>
+ *
+ * This code has been produced on the BGA studio platform for use on http://boardgamearena.com.
+ * See http://en.boardgamearena.com/#!doc/Studio for more information.
+ * -----
+ *
+ * stats.inc.php
+ *
+ * Chess game statistics description
+ *
+ */
+
+/*
+    In this file, you are describing game statistics, that will be displayed at the end of the
+    game.
+    
+    There are 2 types of statistics:
+    _ table statistics, that are not associated to a specific player (ie: 1 value for each game).
+    _ player statistics, that are associated to each players (ie: 1 value for each player in the game).
+
+    Statistics types can be "int" for integer, and "float" for floating point values.
+    
+    Once you defined your statistics there, you can start using "initStat", "setStat" and "incStat" method
+    in your game logic, using statistics names defined below.
+    
+    !! It is not a good idea to modify this file when a game is running !!
+*/
+
+$stats_type = array(
+
+    // Statistics global to table
+    "table" => array(
+
+        "turns_number" => array("id"=> 10,
+                    "name" => totranslate("Number of turns"),
+                    "type" => "int" ),
+
+/*
+        Examples:
+
+
+        "table_teststat1" => array(   "id"=> 10,
+                                "name" => totranslate("table test stat 1"), 
+                                "type" => "int" ),
+                                
+        "table_teststat2" => array(   "id"=> 11,
+                                "name" => totranslate("table test stat 2"), 
+                                "type" => "float" )
+*/  
+    ),
+    
+    // Statistics existing for each player
+    "player" => array(
+
+        "turns_number" => array("id"=> 10,
+                    "name" => totranslate("Number of turns"),
+                    "type" => "int" ),
+    
+/*
+        Examples:    
+        
+        
+        "player_teststat1" => array(   "id"=> 10,
+                                "name" => totranslate("player test stat 1"), 
+                                "type" => "int" ),
+                                
+        "player_teststat2" => array(   "id"=> 11,
+                                "name" => totranslate("player test stat 2"), 
+                                "type" => "float" )
+
+*/    
+    )
+
+);
